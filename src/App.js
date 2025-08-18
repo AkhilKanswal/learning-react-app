59e3df10c15ec4e33c42b7dba22a6e153ed920c6
AkhilKanswal <kanswalakhil20@gmail.com>
2025-08-18 20:20:35 +0530

    Completed the concepts of static pages in react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-
+import Navbar from "./components/Navbar.js"
+import Hero from "./components/Hero.js"
+import Card from "./components/Card.js"
 
 export default function App(){
     return (
-        <h1>Hello world</h1>
+        <div>
+            <Navbar/>
+            <Hero/>
+            <Card
+                img="logo192.png"
+                rating="5.0"
+                reviewCount="6"
+                country="USA"
+                title="Life Lessons from Kaite Zaffers"
+                price={136}
+            />
+        </div>
     )
 }
